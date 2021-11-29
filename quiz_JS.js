a3ffb3341f0a00c80149efeb27d5bddf25a8988b
OttoKase <otto.kase01@gmail.com>
2021-11-30 00:17:34 +0200

    ee proovime?

--- quiz_JS.js
+++ quiz_JS.js
@@ -1,3 +1,6 @@
+// Kasutasin teise inimese loomingut nii-öelda mallina. Originaali autor Danube Phan. Töö
+// leitav leheküljel https://codepen.io/danubevictoria/pen/WObGGQ
+
 // A personality quiz
 
 // This is an array of objects that stores the personality trait that is prompted to the user and the weight for each prompt. 
@@ -6,64 +9,29 @@
 
 var prompts = [
 {
-	prompt: 'I find it difficult to introduce myself to people',
+	prompt: '1. Kui sa näed koera, siis kas sa ka tervitad teda?',
 	weight: -1,
 	class: 'group0'
 },
 {
-	prompt: 'I get so lost in my thoughts I ignore or forget my surroundings',
+	prompt: '2. Kas su parim sõber suudaks su armukest üle võtta?',
 	weight: -1,
 	class: 'group1'
 },
 {
-	prompt: 'I do not usually initiate conversations',
+	prompt: '3. Kas sa oled alkoholi vastu?',
 	weight: -1,
 	class: 'group2'
 },
 {
-	prompt: 'I prefer not to engage with people who seem angry or upset',
+	prompt: '4. Kas sa eelistad pitsat kanepile?',
 	weight: -1,
 	class: 'group3'
 },
 {
-	prompt: 'I choose my friends carefully',
+	prompt: '5. Kas sulle meeldivad lillepoed?',
 	weight: -1,
 	class: 'group4'
-},
-{
-	prompt: 'I find it difficult to tell stories about myself',
-	weight: -1,
-	class: 'group5'
-},
-{
-	prompt: 'I am usually highly motivated and energetic',
-	weight: 1,
-	class: 'group6'
-},
-{
-	prompt: 'I find it easy to walk up to a group of people and join in conversation',
-	weight: 1,
-	class: 'group7'
-},
-{
-	prompt: 'Being adaptable is more important than being organized',
-	weight: 1,
-	class: 'group8'
-},
-{
-	prompt: 'I care more about making sure no one gets upset than winning a debate',
-	weight: 1,
-	class: 'group9'
-},
-{
-	prompt: 'I often do not feel I have to justify myself to people',
-	weight: 1,
-	class: 'group10'
-},
-{
-	prompt: 'I would rather improvise than spend time coming up with a detailed plan',
-	weight: 1,
-	class: 'group11'
 }
 
 ]
@@ -72,27 +40,27 @@ var prompts = [
 // The stronger agreeance/disagreeance, the higher the weight on the user's answer to the prompt.
 var prompt_values = [
 {
-	value: 'Strongly Agree', 
+	value: 'Kindlasti', 
 	class: 'btn-default btn-strongly-agree',
 	weight: 5
 },
 {
-	value: 'Agree',
+	value: 'Pigem jah',
 	class: 'btn-default btn-agree',
 	weight: 3,
 }, 
 {
-	value: 'Neutral', 
+	value: 'Ei oska öelda', 
 	class: 'btn-default',
 	weight: 0
 },
 {
-	value: 'Disagree',
+	value: 'Pigem ei',
 	class: 'btn-default btn-disagree',
 	weight: -3
 },
 { 
-	value: 'Strongly Disagree',
+	value: 'Kindlasti mitte',
 	class: 'btn-default btn-strongly-disagree',
 	weight: -5
 }
@@ -226,29 +194,22 @@ $('#submit-btn').click(function () {
 		// document.getElementById('intro-bar').style.width = ((total / 60) * 100) + '%';
 		// console.log(document.getElementById('intro-bar').style.width);
 		// document.getElementById('intro-bar').innerHTML= ((total / 60) * 100) + '%';
-		document.getElementById('results').innerHTML = '<b>You are introverted!</b><br><br>\
-		Introverts are tricky to understand, since it’s so easy for us to assume that introversion is the same as being shy, when, in fact, introverts are simply people who find it tiring to be around other people.\n\
-<br><br>\
-I love this explanation of an introvert’s need to be alone:\n\
-<br><br>\
-For introverts, to be alone with our thoughts is as restorative as sleeping, as nourishing as eating.\n\n\
-<br><br>\
-Introverted people are known for thinking things through before they speak, enjoying small, close groups of friends and one-on-one time, needing time alone to recharge, and being upset by unexpected changes or last-minute surprises. Introverts are not necessarily shy and may not even avoid social situations, but they will definitely need some time alone or just with close friends or family after spending time in a big crowd.\
-		';
+		document.getElementById('results').innerHTML = '<b>Sa oled Johnny!</b><br><br>\
+		Oled tavaline, armastav tõeline ameeriklane, keda ristavad ebaõiglaselt su hull manipuleeriv emane kihlatu Lisa ja parim sõber Mark.\
+		<br><br>\
+		Sul on kindlasti õigus ja hea. See rämps ei vääri sind. Ta pole isegi nii ilus.\
+		<br><br>\
+		Vean kihla, et ta pole ka atraktiivne. Lollpea. Las ta teeb, mida hing ihkab.';
 	} else if(total > 0) {
-		document.getElementById('results').innerHTML = '<b>You are extroverted!</b><br><br>\
-		On the opposite side of the coin, people who are extroverted are energized by people. They usually enjoy spending time with others, as this is how they recharge from time spent alone focusing or working hard.\
-<br><br>\
-I like how this extrovert explains the way he/she gains energy from being around other people:\
-<br><br>\
-When I am among people, I make eye contact, smile, maybe chat if there’s an opportunity (like being stuck in a long grocery store line). As an extrovert, that’s a small ‘ping’ of energy, a little positive moment in the day.';
+		document.getElementById('results').innerHTML = '<b>Sa oled Mark!</b><br><br>\
+		Johnny parim sõber. Sa oled tema parim sõber. Kas ma pean sulle meelde tuletama, et sa oled Johnny parim sõber?\
+		<br><br>\
+		Sa oled ilus, valge, hingedega mees ja Johnny parim sõber.\
+		<br><br>\
+		Sa oled lihtsalt kõige kenam, kõige silmapaistvam tüüp, nii tore, et petsid oma parima sõbra kihlatu Lisaga. Ta oli su parim sõber (parim sõber).';
 	} else {
-		document.getElementById('results').innerHTML = '<b>You are ambiverted!</b><br><br>\
-		Since introverts and extroverts are the extremes of the scale, the rest of us fall somewhere in the middle. Many of us lean one way or the other, but there are some who are quite balanced between the two tendencies. These people are called ambiverts.\
-<br><br>\
-So let’s look at how an ambivert compares.\
-<br><br>\
-Ambiverts exhibit both extroverted and introverted tendencies. This means that they generally enjoy being around people, but after a long time this will start to drain them. Similarly, they enjoy solitude and quiet, but not for too long. Ambiverts recharge their energy levels with a mixture of social interaction and alone time.'
+		document.getElementById('results').innerHTML = '<b>Sa oled Doggy!</b><br><br>\
+		Sa oled koer. Sul on poogen kõigest. Sa lihtsalt naudid elu. Vahepeal mingid kutid paitavad sind. Mis seal ikka.'
 	}
 
 	// Hide the quiz after they submit their results
